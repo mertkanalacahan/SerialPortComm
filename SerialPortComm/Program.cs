fb5d54bd0a88e1d93b1e0faeed3d61a1f38d63ab
Mertkan Alacahan <mertkanalacahan@gmail.com>
2021-11-12 12:12:20 +0300

    Command 1 being sent with CRC check

--- SerialPortComm/Program.cs
+++ SerialPortComm/Program.cs
@@ -29,18 +29,18 @@ public class SerialPortCommApp
         XmlNodeList dataBits = config.GetElementsByTagName("dataBits");
         XmlNodeList stopBits = config.GetElementsByTagName("stopBits");
         XmlNodeList handshake = config.GetElementsByTagName("handshake");
+        XmlNodeList readTimeout = config.GetElementsByTagName("readTimeout");
+        XmlNodeList writeTimeout = config.GetElementsByTagName("writeTimeout");
 
-        // Set the appropriate properties.
+        // Set the appropriate properties
         _serialPort.PortName = SetPortName(portName[0].InnerText);
         _serialPort.BaudRate = SetPortBaudRate(baudRate[0].InnerText);
         _serialPort.Parity = SetPortParity(parity[0].InnerText);
         _serialPort.DataBits = SetPortDataBits(dataBits[0].InnerText);
         _serialPort.StopBits = SetPortStopBits(stopBits[0].InnerText);
         _serialPort.Handshake = SetPortHandshake(handshake[0].InnerText);
-
-        // Set the read/write timeouts
-        _serialPort.ReadTimeout = 500;
-        _serialPort.WriteTimeout = 500;
+        _serialPort.ReadTimeout = SetReadTimeout(readTimeout[0].InnerText);
+        _serialPort.WriteTimeout = SetWriteTimeout(writeTimeout[0].InnerText);
 
         _serialPort.Open();
         _continue = true;
@@ -59,12 +59,32 @@ public class SerialPortCommApp
                 //“CRC_OK” girmesi durumunda Tablo 2’de tanımlı olan hazır mesajı uygun CRC ile 
                 //alıcıya gönderecektir.
                 //Sunucu tarafı gönderilen ve alınan herbir mesajı aşağıdaki formatlarda ekrana basacaktır.
+                List<byte> bytes = CreateByteListFromCommandFile("command1.xml");
+
+                //calculate and add crc
+                ushort crc = CalculateCRC(bytes.ToArray());
+                bytes.Add((byte)(crc >> 8));
+                bytes.Add((byte)(crc));
+
+                //print message on screen
+
+                _serialPort.WriteLine(Encoding.GetEncoding(28591).GetString(bytes.ToArray()));
             }
             else if (stringComparer.Equals("CRC_ER", command))
             {
                 //“CRC_ER” girmesi durumunda Tablo 2’de tanımlı olan hazır mesajı hatalı CRC ile 
                 //alıcıya gönderecektir.
                 //Sunucu tarafı gönderilen ve alınan herbir mesajı aşağıdaki formatlarda ekrana basacaktır.
+                List<byte> bytes = CreateByteListFromCommandFile("command1.xml");
+
+                //add wrong crc values
+                ushort crc = CalculateCRC(bytes.ToArray());
+                bytes.Add((byte)((crc / 2) >> 8));
+                bytes.Add((byte)((crc / 4)));
+
+                //print message on screen
+
+                _serialPort.WriteLine(Encoding.GetEncoding(28591).GetString(bytes.ToArray()));
             }
             else
             {
@@ -87,18 +107,51 @@ public class SerialPortCommApp
 
                 if (incomingBytes.Length > 0)
                 {
-                    if (incomingBytes[2] == 0xA5)
+                    //CRC check
+                    List<byte> bytesMinusCRC = new List<byte>();
+                    for (int i = 0; i < incomingBytes.Length - 2; i++)
+                        bytesMinusCRC.Add(incomingBytes[i]);
+
+                    ushort crcInMessage = (ushort)((incomingBytes[incomingBytes.Length - 2] << 8) 
+                        + incomingBytes[incomingBytes.Length - 1]);
+
+                    if (crcInMessage != CalculateCRC(bytesMinusCRC.ToArray()))
                     {
-                        Console.WriteLine("gecersiz istek");
-                    }
-                    else
-                    {
-                        //create error message
+                        //create wrong crc error message
                         List<byte> bytes = new List<byte>();
                         bytes.Add(0xCA);
                         bytes.Add(0x05);
                         bytes.Add(0xA5);
-                        bytes.Add(0x02);
+                        bytes.Add(0x01); //wrong crc
+                        ushort crc = CalculateCRC(bytes.ToArray());
+                        bytes.Add((byte)(crc >> 8));
+                        bytes.Add((byte)(crc));
+
+                        //send message
+                        _serialPort.WriteLine(Encoding.GetEncoding(28591).GetString(bytes.ToArray()));
+                    }
+                    else if (incomingBytes[2] == 0xA5)
+                    {
+                        Console.WriteLine("gecersiz istek");
+                        //yalnızca ekrana bas
+                    }
+                    else if (incomingBytes[2] == 0xA9)
+                    {
+                        Console.WriteLine("Komut 2");
+                        //yalnızca ekrana bas
+                    }
+                    else if (incomingBytes[2] == 0xA8)
+                    {
+                        Console.WriteLine("Komut 1");
+                    }
+                    else
+                    {
+                        //create invalid request error message
+                        List<byte> bytes = new List<byte>();
+                        bytes.Add(0xCA);
+                        bytes.Add(0x05);
+                        bytes.Add(0xA5);
+                        bytes.Add(0x02); //invalid request
                         ushort crc = CalculateCRC(bytes.ToArray());
                         bytes.Add((byte)(crc >> 8));
                         bytes.Add((byte)(crc));
@@ -112,7 +165,6 @@ public class SerialPortCommApp
         }
     }
 
-    // Display Port values and prompt user to enter a port.
     public static string SetPortName(string portName)
     {
         if (portName == "" || !(portName.ToLower()).StartsWith("com"))
@@ -122,7 +174,7 @@ public class SerialPortCommApp
 
         return portName;
     }
-    // Display BaudRate values and prompt user to enter a value.
+
     public static int SetPortBaudRate(string baudRate)
     {
         if (baudRate == "")
@@ -133,7 +185,6 @@ public class SerialPortCommApp
         return int.Parse(baudRate);
     }
 
-    // Display PortParity values and prompt user to enter a value.
     public static Parity SetPortParity(string parity)
     {
         if (parity == "")
@@ -143,7 +194,7 @@ public class SerialPortCommApp
 
         return (Parity)Enum.Parse(typeof(Parity), parity, true);
     }
-    // Display DataBits values and prompt user to enter a value.
+
     public static int SetPortDataBits(string dataBits)
     {
         if (dataBits == "")
@@ -154,7 +205,6 @@ public class SerialPortCommApp
         return int.Parse(dataBits.ToUpperInvariant());
     }
 
-    // Display StopBits values and prompt user to enter a value.
     public static StopBits SetPortStopBits(string stopBits)
     {
         if (stopBits == "")
@@ -164,6 +214,7 @@ public class SerialPortCommApp
 
         return (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
     }
+
     public static Handshake SetPortHandshake(string handshake)
     {
         if (handshake == "")
@@ -173,6 +224,26 @@ public class SerialPortCommApp
 
         return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
     }
+
+    public static int SetReadTimeout(string readTimeout)
+    {
+        if (readTimeout == "")
+        {
+            readTimeout = "500";
+        }
+
+        return int.Parse(readTimeout);
+    }
+    public static int SetWriteTimeout(string writeTimeout)
+    {
+        if (writeTimeout == "")
+        {
+            writeTimeout = "500";
+        }
+
+        return int.Parse(writeTimeout);
+    }
+
     private static ushort CalculateCRC(byte[] data)
     {
         ushort wCRC = 0;
@@ -188,5 +259,38 @@ public class SerialPortCommApp
             }
         }
         return wCRC;
+    }
+
+    private static List<byte> CreateByteListFromCommandFile(string filename)
+    {
+        XmlDocument command = new XmlDocument();
+        command.Load(filename);
+
+        XmlNodeList header = command.GetElementsByTagName("header");
+        XmlNodeList msgLength = command.GetElementsByTagName("msgLength");
+        XmlNodeList commandNo = command.GetElementsByTagName("commandNo");
+        XmlNodeList byteData = command.GetElementsByTagName("UInt8");
+        XmlNodeList intData = command.GetElementsByTagName("UInt32");
+
+        byte headerByte = byte.Parse(header[0].InnerText);
+        byte msgLengthByte = byte.Parse(msgLength[0].InnerText);
+        byte commandNoByte = byte.Parse(commandNo[0].InnerText);
+        byte byteDataByte = byte.Parse(byteData[0].InnerText);
+        uint integer = uint.Parse(intData[0].InnerText);
+
+        List<byte> bytes = new List<byte>();
+        bytes.Add(headerByte);
+        bytes.Add(msgLengthByte);
+        bytes.Add(commandNoByte);
+        bytes.Add(byteDataByte);
+
+        byte[] intBytes = BitConverter.GetBytes(integer);
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(intBytes);
+
+        foreach (byte elem in intBytes)
+            bytes.Add(elem);
+
+        return bytes;
     }
 }
